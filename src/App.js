c0df3637322397bbc3f6628f2daf92fefd13e31d
Harsh Thakur <90609542+harshthakur2021@users.noreply.github.com>
2024-08-20 14:58:02 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 import * as tf from "@tensorflow/tfjs";
-import Webcam from "react-webcam";
 import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
 import { drawMesh } from "./utilities";
+// Correct import for the background image
+
+import bgimage from './public/bgimage.jpg';
 
 function App() {
-  // Setup references
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  const [isModelActive, setIsModelActive] = useState(false);
 
-  // Load Facemesh
   const runFacemesh = async () => {
     const net = await facemesh.load({
       inputResolution: { width: 640, height: 480 },
       scale: 0.8,
     });
+    
     setInterval(() => {
       detect(net);
     }, 100);
@@ -27,10 +30,9 @@ function App() {
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
-      // Get Video Properties
       const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
 
       // Set video width
       webcamRef.current.video.width = videoWidth;
@@ -41,52 +43,85 @@ function App() {
       canvasRef.current.height = videoHeight;
 
       // Make detections
-      const face= await net.estimateFaces(video);
-      console.log(face);
-      
+      const face = await net.estimateFaces({ input: video });
+
       // Get canvas context
       const ctx = canvasRef.current.getContext("2d");
-      requestAnimationFrame(()=>{drawMesh(face, ctx)});
+      drawMesh(face, ctx);
     }
   };
 
-  // Run Facemesh after component mounts
-  useEffect(() => {
+  const handleButtonClick = () => {
+    setIsModelActive(true);
     runFacemesh();
-  }, []);
-runFacemesh();  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zIndex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+  };
 
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zIndex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <header className="App-header">
+        {!isModelActive && (
+          <button
+            onClick={handleButtonClick}
+            style={{
+              padding: "10px 20px",
+              fontSize: "18px",
+              backgroundColor: "#4CAF50",
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+          >
+            Start Face Mesh
+          </button>
+        )}
+        {isModelActive && (
+          <div
+            style={{
+              position: "relative",
+              width: 640,
+              height: 480,
+            }}
+          >
+            <Webcam
+              ref={webcamRef}
+              style={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                textAlign: "center",
+                zIndex: 9,
+                width: 640,
+                height: 480,
+              }}
+            />
+
+            <canvas
+              ref={canvasRef}
+              style={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                textAlign: "center",
+                zIndex: 9,
+                width: 640,
+                height: 480,
+              }}
+            />
+          </div>
+        )}
       </header>
     </div>
   );
